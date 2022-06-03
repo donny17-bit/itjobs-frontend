41aa23f8fdb5cb1a2d43a827295c4c32f0f5bf43
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-03 17:17:54 +0700

    Create redux store, import bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../scss/customBootstrap.min.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Store from "../store";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("../node_modules/bootstrap/dist/js/bootstrap")
+      : null;
+  }, []);
+
+  return (
+    <Provider store={Store.store}>
+      <PersistGate loading={null} persistor={Store.persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
