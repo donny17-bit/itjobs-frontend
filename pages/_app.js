fb8ae3c8838924eefe65e56a66f31ca72594b012
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-04 19:43:33 +0700

    Import bootstrap icon to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../scss/customBootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Store from "../store";
