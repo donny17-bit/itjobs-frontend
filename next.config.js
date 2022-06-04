cbebec899ab9951c129a321c8142d575f56f5b14
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-04 19:35:28 +0700

    Add env and allowed image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    URL_BACKEND: "",
+    URL_CLOUDINARY:
+      "https://res.cloudinary.com/itjobs/image/upload/v1654266716/",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
