5e234235901813bd468d8f7c1fb77cfbd34d11fe
Qxtlp <xi.tulop@yandex.com>
2022-06-06 15:22:35 +0700

    add integration

--- pages/hire/[id].jsx
+++ pages/hire/[id].jsx
@@ -1,19 +1,53 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { useDispatch } from "react-redux";
+import Cookie from "js-cookie";
+import { useDispatch, useSelector } from "react-redux";
 import { postHire } from "../../store/action/hire";
+import { useRouter } from "next/router";
+import { getSkill, getUserById } from "../../store/action/user";
 
 function Hire() {
+  const router = useRouter();
   const dispatch = useDispatch();
   const [form, setForm] = useState({
     subject: "",
     description: "",
     file: null,
-    userId: "73e8deec-9504-4916-aa1b-c522cedd2aeb",
+    userId: router.query.id,
   });
+  const [data, setData] = useState({});
 
-  useEffect(() => {}, []);
+  console.log(router.query);
+
+  useEffect(() => {
+    if (router.isReady) {
+      getData();
+      console.log("object");
+    }
+  }, [router.isReady]);
+
+  console.log(data);
+
+  const getData = () => {
+    try {
+      dispatch(getUserById(router.query.id))
+        .then((res) => {
+          const dataUser = res.value.data.data[0];
+
+          dispatch(getSkill(router.query.id))
+            .then((res) =>
+              setData({
+                ...dataUser,
+                skill: res.value.data.data.map((v) => v.skill),
+              })
+            )
+            .catch((err) => alert(err));
+        })
+        .catch((err) => alert(err));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleChangeForm = ({ target }) => {
     if (target.name == "file") {
@@ -22,7 +56,6 @@ function Hire() {
       setForm({ ...form, [target.name]: target.value });
     }
   };
-  console.log(form);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -35,7 +68,7 @@ function Hire() {
       console.log(data[0] + ": " + data[1]);
     }
 
-    dispatch(postHire("86640e72-9499-44ab-aa15-f9eea4511de9", formData))
+    dispatch(postHire(Cookie.get("id"), formData))
       .then((res) => alert("success"))
       .catch((err) => alert(err));
   };
@@ -46,7 +79,11 @@ function Hire() {
         <div className="col-12 col-md-3 bg-white px-4 pb-5 rounded">
           <div className="position-relative px-5 py-3 text-center">
             <Image
-              src={"/vercel.svg"}
+              src={
+                data?.image
+                  ? `${process.env.URL_CLOUDINARY}${data.image}`
+                  : `${process.env.URL_CLOUDINARY}profiles/profile-placeholder_zpfgnr.png`
+              }
               layout="intrinsic"
               objectFit="cover"
               className="rounded-circle"
@@ -55,10 +92,10 @@ function Hire() {
               alt=""
             ></Image>
           </div>
-          <h4>Louis Tomlinson</h4>
-          <h6 className="m-0">Web Developer</h6>
-          <small className="text-black-50">
-            Freelancer
+          <h4 className="fw-bold">{data?.fullName}</h4>
+          <h6 className="m-0 text-capitalize">{data?.field}</h6>
+          <small className="text-black-50 ">
+            <span className="text-capitalize">{data?.role}</span>
             <br />
             <div className="mt-2 mb-2">
               <svg
@@ -72,35 +109,37 @@ function Hire() {
                 <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
                 <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
               </svg>
-              Purwokerto
+              {data?.address}
             </div>
-            <div className="mt-2 mb-2">088-080808</div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum erat orci, mollis nec gravida sed, ornare quis urna.
-              Curabitur eu lacus fringilla, vestibulum risus at.
-            </p>
+            <div className="mt-2 mb-2">
+              {(data?.noTelp || "")
+                .toString()
+                .replace(/((?<!\d)\d{3}(?!\b)|(?<=^\d{3})\d{4}(?!\b))/g, "$1-")}
+            </div>
+            <p>{data?.description}</p>
           </small>
           <h4>Skill</h4>
-          <div className="d-flex flex-wrap" style={{}}>
-            {["python", "javascript", "C#", "php"].map((v, i) => (
-              <React.Fragment key={i}>
-                <div>
-                  <button className=" btn btn-sm bg-warning text-white p-1 px-md-3 py-1 me-2 bg-opacity-50 border border-warning">
-                    {console.log((i + 1) / 3 == 1 ? true : false)}
-                    {v}
-                  </button>
-                </div>
-                {(i + 1) % 3 == 0 ? <div className="w-100 mt-2"></div> : ""}
-              </React.Fragment>
-            ))}
+          <div className="d-flex flex-wrap fw-light" style={{}}>
+            {data?.skill
+              ? data.skill.map((v, i) => (
+                  <React.Fragment key={i}>
+                    <div>
+                      <button className=" btn btn-sm bg-warning text-white p-1 px-md-3 py-1 me-2 bg-opacity-50 border border-warning">
+                        {console.log((i + 1) / 3 == 1 ? true : false)}
+                        {v}
+                      </button>
+                    </div>
+                    {(i + 1) % 3 == 0 ? <div className="w-100 mt-2"></div> : ""}
+                  </React.Fragment>
+                ))
+              : "apparently s/he doesn't have any skill"}
           </div>
         </div>
         <form
           onSubmit={(event) => handleSubmit(event)}
           className="col-12 col-md-8 mt-4 mt-md-0 ms-md-5 "
         >
-          <h2 className="fw-bold">Hubungi Louis TOmlinson</h2>
+          <h2 className="fw-bold">Hubungi {data?.fullName}</h2>
           <small className="fw-light">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
             ipsum et dui rhoncus auctor.
