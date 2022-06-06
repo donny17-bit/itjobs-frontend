be64b1b996e59dc2aff0f3319d942b73757a0639
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-06 12:23:58 +0700

    Make landingpage responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,43 +4,41 @@ import Image from "next/image";
 import TestiCard from "../components/TestiCard";
 
 function LandingPage() {
-  const testiDataRow1 = [
+  const testiData = [
     {
-      name: "Harry Styles",
+      name: "Gavin Alter",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.",
     },
     {
-      name: "Niall Horan",
+      name: "Nonso Almiro",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      name: "Louis Tomlinson",
+      name: "Chika Maitland",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial: "Lorem ipsum dolor sit amet, consectetur ",
     },
-  ];
-  const testiDataRow2 = [
     {
-      name: "Harry Styles",
+      name: "Jameson Emilis",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.",
     },
     {
-      name: "Niall Horan",
+      name: "Adeola Katlega",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      name: "Louis Tomlinson",
+      name: "Amit Chibuzo",
       job: "Web Developer",
       image: "/image/profile-placeholder.png",
       testimonial: "Lorem ipsum dolor sit amet, consectetur ",
@@ -52,7 +50,7 @@ function LandingPage() {
       <section className="container-fluid bg-white">
         <div className="container-lg py-5">
           <div className="row">
-            <div className="col-6 px-5 d-flex flex-column justify-content-center align-items-start">
+            <div className="order-2 order-sm-1 col-sm-7 col-md-6 px-3 px-md-5 d-flex flex-column justify-content-center align-items-center align-items-sm-start text-center text-sm-start">
               <h2 className="fs-1 fw-semibold mb-3">
                 Talenta terbaik negeri untuk perubahan revolusi 4.0
               </h2>
@@ -64,7 +62,7 @@ function LandingPage() {
                 Mulai Dari Sekarang
               </button>
             </div>
-            <div className="col-6 text-center">
+            <div className="order-1 order-sm-2 col-sm-5 col-md-6 text-center mb-4 mb-sm-0">
               <Image
                 src="/image/landing1.png"
                 alt="ilustrasi pekerja"
@@ -77,10 +75,13 @@ function LandingPage() {
         </div>
       </section>
 
-      <section className="container-fluid bg-white">
+      <section
+        className="container-fluid bg-white"
+        style={{ backgroundColor: "var(--color-background)" }}
+      >
         <div className="container-lg py-5">
           <div className="row">
-            <div className="col-5">
+            <div className="col-sm-5 d-none d-sm-flex align-items-center">
               <Image
                 src="/image/landing2.png"
                 alt="pekerja"
@@ -89,21 +90,21 @@ function LandingPage() {
                 objectFit="contain"
               />
             </div>
-            <div className="col-7 px-5">
-              <h2 className="fw-semibold lh-base mb-4">
-                Kenapa harus mencari tallent di{" "}
+            <div className="col-sm-7 px-3 px-md-5 d-flex flex-column justify-content-center align-items-center align-items-sm-start">
+              <h2 className="fw-semibold mb-3 mb-md-4 text-center text-sm-start">
+                Kenapa harus mencari talent di{" "}
                 <span className="text-primary fw-bold">itJobs</span>?
               </h2>
               <ul className="list-unstyled">
-                <li className="mb-4">
+                <li className="mb-2 mb-md-4">
                   <i className="bi bi-check-circle-fill text-primary me-3"></i>
                   Lorem ipsum dolor sit amet
                 </li>
-                <li className="mb-4">
+                <li className="mb-2 mb-md-4">
                   <i className="bi bi-check-circle-fill text-primary me-3"></i>
                   Lorem ipsum dolor sit amet
                 </li>
-                <li className="mb-4">
+                <li className="mb-2 mb-md-4">
                   <i className="bi bi-check-circle-fill text-primary me-3"></i>
                   Lorem ipsum dolor sit amet
                 </li>
@@ -120,24 +121,26 @@ function LandingPage() {
       <section className="container-fluid bg-white">
         <div className="container-lg py-5">
           <div className="row">
-            <div className="col-6 px-5 d-flex flex-column justify-content-center">
-              <h2 className="fw-semibold lh-base mb-2">Skill Tallent</h2>
-              <p className="opacity-75 mb-4">
+            <div className="col-sm-6 px-3 px-md-5 d-flex flex-column justify-content-center align-items-center align-items-sm-start">
+              <h2 className="fw-semibold mb-2 text-center text-sm-left">
+                Skill Tallent
+              </h2>
+              <p className="opacity-75 mb-4 text-center text-sm-start">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
                 euismod ipsum et dui rhoncus auctor.
               </p>
-              <ul className="list-unstyled">
+              <ul className="list-unstyled text-nowrap">
                 <div className="row">
-                  <div className="col-6">
-                    <li className="mb-4">
+                  <div className="col-5">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       Java
                     </li>
-                    <li className="mb-4">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       Kotlin
                     </li>
-                    <li className="mb-4">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       PHP
                     </li>
@@ -146,16 +149,16 @@ function LandingPage() {
                       Javascript
                     </li>
                   </div>
-                  <div className="col-6">
-                    <li className="mb-4">
+                  <div className="col-7">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       Golang
                     </li>
-                    <li className="mb-4">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       C++
                     </li>
-                    <li className="mb-4">
+                    <li className="mb-2 mb-md-4">
                       <i className="bi bi-check-circle-fill text-warning me-3"></i>
                       Ruby
                     </li>
@@ -167,7 +170,7 @@ function LandingPage() {
                 </div>
               </ul>
             </div>
-            <div className="col-6">
+            <div className="col-sm-6 d-none d-sm-flex align-items-center">
               <Image
                 src="/image/landing3.png"
                 alt="pekerja"
@@ -185,7 +188,7 @@ function LandingPage() {
         style={{ backgroundColor: "var(--color-background)" }}
       >
         <div className="container-lg py-5">
-          <h2 className="text-center mb-5">
+          <h2 className="text-center mb-4">
             Their opinion about{" "}
             <span className="fw-bold text-primary">itJobs</span>
           </h2>
@@ -196,18 +199,24 @@ function LandingPage() {
           >
             <div className="carousel-inner px-3">
               <div className="carousel-item active">
-                <div className="row">
-                  {testiDataRow1.map((testi, index) => (
-                    <div className="col-4 px-3 px-md-4 py-4 d-flex" key={index}>
+                <div className="row landing_testi">
+                  {testiData.slice(0, 3).map((testi, index) => (
+                    <div
+                      className="col-sm-6 col-md-4 px-3 px-md-4 py-4 d-flex"
+                      key={index}
+                    >
                       <TestiCard data={testi} />
                     </div>
                   ))}
                 </div>
               </div>
               <div className="carousel-item">
-                <div className="row">
-                  {testiDataRow2.map((testi, index) => (
-                    <div className="col-4 px-3 px-md-4 py-4 d-flex" key={index}>
+                <div className="row landing_testi">
+                  {testiData.slice(3, 6).map((testi, index) => (
+                    <div
+                      className="col-sm-6 col-md-4 px-3 px-md-4 py-4 d-flex"
+                      key={index}
+                    >
                       <TestiCard data={testi} />
                     </div>
                   ))}
@@ -243,9 +252,9 @@ function LandingPage() {
       </section>
 
       <section className="container-fluid">
-        <div className="container-lg py-3py-md-5">
-          <div className="home_mailist bg-primary px-5 py-5 my-5 d-flex justify-content-between align-items-center">
-            <h2 className="fw-semibold text-white">
+        <div className="container-lg py-3 py-md-5">
+          <div className="landing_mailist bg-primary p-5 my-5 text-center text-sm-start d-block d-sm-flex justify-content-between align-items-center">
+            <h2 className="fw-semibold text-white mb-4 mb-sm-0">
               Lorem ipsum <br /> dolor sit amet
             </h2>
             <button role="button" className="btn btn-light px-3 py-2 py-md-3">
